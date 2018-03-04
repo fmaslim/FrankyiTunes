5643ba983df16278a0657a33da60d3c0f80ec215
fmaslim <franky.maslim@yahoo.com>
2018-03-04 00:38:36 -0500

    Added model driven form with shorter validation expressions

--- src/app/forms/model-driven-form/model-driven-form.component.ts
+++ src/app/forms/model-driven-form/model-driven-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-model-driven-form',
@@ -14,18 +14,52 @@ export class ModelDrivenFormComponent implements OnInit {
   ];
 
   myForm: FormGroup;
+  name: FormGroup;
+  firstName: FormControl;
+  lastName: FormControl;
+  email: FormControl;
+  password: FormControl;
+  language: FormControl;
 
   constructor() { }
 
   ngOnInit() {
+
+    // Instead of doing it this way, create the controls separately in a function
+
+    // this.myForm = new FormGroup({
+    //   name: new FormGroup({
+    //     firstName: new FormControl('', Validators.required),
+    //     lastName: new FormControl('', Validators.required)
+    //   }),
+    //   email: new FormControl('john.doe@test.com', Validators.required),
+    //   password: new FormControl('1234567890', Validators.required),
+    //   language: new FormControl('English', Validators.required)
+    // });
+
+    this.createFormControls();
+    this.createForm();
+  }
+
+  createFormControls() {
+    this.firstName = new FormControl('', Validators.required);
+    this.lastName = new FormControl('', Validators.required);
+    this.email = new FormControl('', Validators.required);
+    this.password = new FormControl('', Validators.required);
+    this.language = new FormControl('', Validators.required);
+
+    this.name = new FormGroup({
+      firstName: this.firstName,
+      lastName: this.lastName
+    });
+  }
+
+  createForm() {
     this.myForm = new FormGroup({
-      name: new FormGroup({
-        firstName: new FormControl(),
-        lastName: new FormControl()
-      }),
-      email: new FormControl(),
-      password: new FormControl(),
-      language: new FormControl()
+      name: this.name,
+      email: this.email,
+      password: this.email,
+      language: this.language
     });
   }
 
